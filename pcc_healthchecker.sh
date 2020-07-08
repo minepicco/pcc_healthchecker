37e3fdb38ce0c880ecb7d77410aaf51d780935d9
Minehiko Nohara <minepicco@gmail.com>
2020-07-08 10:57:07 +0900

    Update pcc_healthchecker.sh

--- pcc_healthchecker.sh
+++ pcc_healthchecker.sh
@@ -4,6 +4,6 @@ while true
 do
   curl -k -X GET $PCC_URL"/api/v1/_ping" > result.txt
   if [ `cat result.txt | grep -c "OK"` -eq 1 ]; then TEXT="good"; else TEXT="@channel Prisma Cloudで障害の可能性あり！"; fi
-  curl -X POST -d 'payload={"channel":"'$CHANNEL'","username":"'$NAME'","icon_emoji":"'$ICON'","link_names": "1","attachments": [{"text": "'$TEXT'"}]}' 
+  curl -X POST -d 'payload={"channel":"'$CHANNEL'","username":"'$NAME'","icon_emoji":"'$ICON'","link_names": "1","attachments": [{"text": "'$TEXT'"}]}' $WH_URL
   sleep $ITV
 done
