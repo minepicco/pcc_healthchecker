bce7b733b7e143d7ee14d1556709be7e66effaf8
Minehiko Nohara <minepicco@gmail.com>
2020-12-18 17:22:53 +0900

    Update pcc_healthchecker.sh

--- pcc_healthchecker.sh
+++ pcc_healthchecker.sh
@@ -1,9 +1,10 @@
 #!/bin/bash
 
+d=`date +%d`
 while true
 do
   curl -k -X GET $PCC_URL"/api/v1/_ping" > result.txt
-  if [ `cat result.txt | grep -c "OK"` -eq 1 ]; then TEXT="good"; else TEXT="@channel注意！PrismaCloudで障害の可能性あり！"; fi
-  curl -X POST -d 'payload={"channel":"'$CHANNEL'","username":"'$NAME'","icon_emoji":"'$ICON'","link_names": "1","attachments": [{"text": "'$TEXT'"}]}' $WH_URL
+  if [ `cat result.txt | grep -c "OK"` -eq 1 ]; then TEXT="good"; else TEXT="@channel注意！PrismaCloudで障害の可能性あり！"&&curl -X POST -d 'payload={"channel":"'$CHANNEL'","username":"'$NAME'","icon_emoji":"'$ICON'","link_names": "1","attachments": [{"text": "'$TEXT'"}]}' $WH_URL; fi
+  if [ `date +%d` -eq $d ]; then echo `date`": "$TEXT >> daily.txt; else curl -F file=@daily.txt -F "initial_comment=本日のサマリー" -F channels=nohara-demo -H "Authorization: Bearer "$token https://slack.com/api/files.upload && rm -rf daily.txt && d=`date +%d`; fi
   sleep $ITV
 done
