d387d6f66d9f164ceabffd44af3445e0bdc564a0
Minehiko Nohara <minepicco@gmail.com>
2020-12-21 10:31:42 +0900

    Update pcc_healthchecker.sh

--- pcc_healthchecker.sh
+++ pcc_healthchecker.sh
@@ -5,6 +5,6 @@ while true
 do
   curl -k -X GET $PCC_URL"/api/v1/_ping" > result.txt
   if [ `cat result.txt | grep -c "OK"` -eq 1 ]; then TEXT="good"; else TEXT="@channel注意！PrismaCloudで障害の可能性あり！"&&curl -X POST -d 'payload={"channel":"'$CHANNEL'","username":"'$NAME'","icon_emoji":"'$ICON'","link_names": "1","attachments": [{"text": "'$TEXT'"}]}' $WH_URL; fi
-  if [ `date +%d` -eq $d ]; then echo `date`": "$TEXT >> daily.txt; else curl -F file=@daily.txt -F "initial_comment=本日のサマリー" -F channels=$CHANNEL -H "Authorization: Bearer "$BOT https://slack.com/api/files.upload && rm -rf daily.txt && d=`date +%d`; fi
+  if [ `date +%d` -eq $d ]; then echo `date`": "$TEXT >> daily.txt; else curl -F file=@daily.txt -F "initial_comment=過去24時間のサマリー" -F channels=$CHANNEL -H "Authorization: Bearer "$BOT https://slack.com/api/files.upload && rm -rf daily.txt && d=`date +%d`; fi
   sleep $ITV
 done
